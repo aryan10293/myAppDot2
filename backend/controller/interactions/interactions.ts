e975b0d62e49abedee6d3f8e79d638568a5baf89
aondrej ryan <aondrejryan11@gmail.com>
2025-11-12 16:23:00 -0800

    added checkin logic

--- backend/controller/interactions/interactions.ts
+++ backend/controller/interactions/interactions.ts
@@ -19,55 +19,57 @@ let interactions = {
         }
       },
       checkIn: async (req: Request, res: Response) => {
-        // task for tomorrow
-        // if user goes mor ethan one day with out checking in set strek to 0
-        // if some
         try {
             const userId = (req as any).user.sub;
-
             
             const { rows } = await pool.query(
             "SELECT id, streak, last_checkin, time_zone FROM users WHERE id = $1",
             [userId]
             );
+
             const user = rows[0];
+
             if (!user) return res.status(404).json({ message: "User not found" });
 
-            
-            const tz = user.timezone || "UTC";
-            const lastLocal = new Date(user.last_checkin).toLocaleDateString("en-CA", {
-            timeZone: tz,
-            }); 
-            const nowLocal = new Date().toLocaleDateString("en-CA", { timeZone: tz });
+            const raw = user.last_checkin + ""; // raw
 
-            console.log({ tz, lastLocal, nowLocal });
+            let checkin = DateTime.fromJSDate(new Date(raw)); 
+            let today = DateTime.fromJSDate(new Date())
 
-            
-            if (lastLocal === nowLocal) {
-            return res.status(200).json({
-                updated: false,
-                message: "Already checked in today.",
-                streak: user.streak,
-            });
+            checkin = checkin.toISO().slice(0,10);
+            checkin =  DateTime.fromISO(checkin);
+
+            today = today.toISO().slice(0,10);
+            today = DateTime.fromISO(today);
+
+            const diff = today.diff(checkin, 'days').days
+
+            const updateQuery = `
+                UPDATE users
+                SET streak = $1,
+                    last_checkin = NOW()
+                WHERE id = $2
+                RETURNING *;
+            `;
+
+            if(diff <= 0){
+                return res.status(200).json({
+                    updated: false,
+                    message: "Already checked in today.",
+                    streak: user.streak,
+                });
+            } else {
+                const updateValues = [user.streak + 1, user.id];
+                const result = await pool.query(updateQuery, updateValues);
+
+                
+                return res.status(200).json({
+                updated: true,
+                message: "Check-in successful.",
+                ...result.rows[0],
+                });
             }
 
-            
-            const updateQuery = `
-            UPDATE users
-            SET streak = $1,
-                last_checkin = NOW()
-            WHERE id = $2
-            RETURNING *;
-            `;
-            const updateValues = [user.streak + 1, user.id];
-            const result = await pool.query(updateQuery, updateValues);
-
-            
-            return res.status(200).json({
-            updated: true,
-            message: "Check-in successful.",
-            ...result.rows[0],
-            });
         } catch (error) {
             console.error(error);
             return res.status(500).json({ message: error });
